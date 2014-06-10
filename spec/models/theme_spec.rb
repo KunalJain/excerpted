b657b9c0e6e32b2704254b18bfd69820f7962e12
ndintenfass <nathan@hoteldelta.net>
2014-06-10 00:30:19 -0700

    make the test of Theme more thorough

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -15,13 +15,24 @@ require 'rails_helper'
 
 RSpec.describe Theme, :type => :model do
   
-  let(:theme){described_class.new}
-
-  context "when values are defaults" do
+  context "when basic theme values are defaults" do
     it "should have the sensible default values" do
+      theme = Theme.new
       expect(theme.bgcolor).to eq 'EEEEEE'
       expect(theme.bordercolor).to eq '333333'
       expect(theme.textcolor).to eq '333333'   
+    end    
+  end
+
+  context "when basic theme values are set explicitly" do
+    it "should return the explicitly set values" do
+      theme = Theme.new
+      theme.bgcolor = 'FF0000'
+      theme.bordercolor = '00FF00'
+      theme.textcolor = '0000FF'
+      expect(theme.bgcolor).to eq 'FF0000'
+      expect(theme.bordercolor).to eq '00FF00'
+      expect(theme.textcolor).to eq '0000FF'   
     end    
   end
 
