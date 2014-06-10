84564fe62ac20c719c5e40f9f2577aa8b1ff181e
ndintenfass <nathan@hoteldelta.net>
2014-06-10 00:20:26 -0700

    Theme has sensible defaults and test coverage for those defaults

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,5 +1,28 @@
+# == Schema Information
+#
+# Table name: themes
+#
+#  id          :integer          not null, primary key
+#  bgcolor     :string(6)
+#  bordercolor :string(6)
+#  textcolor   :string(6)
+#  font        :string(6)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Theme, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:theme){described_class.new}
+
+  context "when values are defaults" do
+    it "should have the sensible default values" do
+      expect(theme.bgcolor).to eq 'EEEEEE'
+      expect(theme.bordercolor).to eq '333333'
+      expect(theme.textcolor).to eq '333333'   
+    end    
+  end
+
 end
